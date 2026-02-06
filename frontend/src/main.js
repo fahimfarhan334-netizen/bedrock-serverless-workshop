f6e96b12687f64d82db69fa2bdd21ef9c665406a
Student <student@example.com>
2026-02-06 17:40:48 +0000

    Customize RAG UI sample questions (clickable)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,9 +7,9 @@ import VueAxios from 'vue-axios'
 Vue.config.productionTip = false
 
 //These values are replaces during amplify setup
-axios.defaults.baseURL = "<ApiGatewayUrl>"
-Vue.prototype.$UserPoolId = '<CognitoUserPoolId>'
-Vue.prototype.$ClientId = '<UserPoolClientId>'
+axios.defaults.baseURL = "https://hcwr71ev6j.execute-api.us-west-2.amazonaws.com/prod"
+Vue.prototype.$UserPoolId = 'us-west-2_YSR0ABYTR'
+Vue.prototype.$ClientId = '1rn2ns4lbus60sh8cds797o29b'
 
 Vue.use(VueAxios, axios)
 
